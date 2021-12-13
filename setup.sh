4ffc2241ed858098b7ffabb0036f56891f0f339a
GMX <pbrowning7@gmail.com>
2021-12-13 05:40:04 -0500

    Continue working with filebeat, suricata, snort, and zeek. Current best vector is using logstash input from external snort, and piping into elastic. Filebeat may be useful is snort is local. However, running snort in a docker is proving difficult.

--- setup.sh
+++
@@ -1,4 +0,0 @@
-sudo echo "pulling docker images" >> /home/cisco/docker.log
-sudo docker-compose -f /home/cisco/elknet/docker-compose.yaml pull
-sudo echo "starting containers" >> /home/cisco/docker.log
-sudo docker-compose -f /home/cisco/elknet/docker-compose.yaml up -d
